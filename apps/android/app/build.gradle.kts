1e5a656f767796e575a640fc9058fa1fbe78751a
REiQEr Local <reiqer-local@users.noreply.local>
2026-05-15 00:39:15 +0800

    chore(native): 替换开屏图，删除旧版本

--- apps/android/app/build.gradle.kts
+++ apps/android/app/build.gradle.kts
@@ -27,7 +27,7 @@ android {
         create("native") {
             dimension = "variant"
             applicationId = "com.reqir.shirohaquiz"
-            versionCode = 51
+            versionCode = 53
             versionName = "0.3.0"
         }
     }
