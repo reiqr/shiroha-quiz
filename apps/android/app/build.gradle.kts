e31913ccf5b396d7cbb386395364b3994c39e40b
REiQEr Local <reiqer-local@users.noreply.local>
2026-05-28 12:54:42 +0800

    chore: bump native to v0.6.3

--- apps/android/app/build.gradle.kts
+++ apps/android/app/build.gradle.kts
@@ -27,8 +27,8 @@ android {
         create("native") {
             dimension = "variant"
             applicationId = "com.reqir.shirohaquiz"
-            versionCode = 151
-            versionName = "0.6.2"
+            versionCode = 152
+            versionName = "0.6.3"
         }
     }
 
