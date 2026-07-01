27b13f3036bf22cf9ce964436db797d4c039d3d1
REiQEr Local <reiqer-local@users.noreply.local>
2026-07-01 21:27:22 +0800

    tweak: versionCode bump 195→196

--- apps/android/app/build.gradle.kts
+++ apps/android/app/build.gradle.kts
@@ -27,7 +27,7 @@ android {
         create("native") {
             dimension = "variant"
             applicationId = "com.reqir.shirohaquiz"
-            versionCode = 195
+            versionCode = 196
             versionName = "0.9.5.5"
         }
     }
